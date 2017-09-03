ca23985bd51572addb7ec770aa4c2f1115940a5b
Mehrdad Dadkhah <mehrdad@dadkhah.me>
2017-09-03 12:28:18 +0430

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # php-fine-uploader
-php lib for use fine-uploader js as client and upload big files with separate to chunks files
+
+
+[![Software License](https://img.shields.io/badge/license-GPL-brightgreen.svg?style=flat-square)](LICENSE)
+[![Packagist Version](https://img.shields.io/packagist/v/Mehrdad-Dadkhah/php-fine-uploader.svg?style=flat-square)](https://packagist.org/packages/mehrdad-dadkhah/php-fine-uploader)
+
+
+php lib for use [fine-uploader js](https://fineuploader.com/) as client and upload big files with separate to chunks files.
+This libarary is base on [chunks-uploader](https://github.com/Mehrdad-Dadkhah/chunks-uploader).
+
+
+## Installation
+
+```
+composer require mehrdad-dadkhah/php-fine-uploader
+```
+
+## Usage
+
+```PHP
+use MehrdadDadkhah\Video\FineUploader;
+
+$uploaderService = new FineUploader();
+$uploadResult = $uploaderService->setConfigs(
+                'form-input-name',
+                'path-to-chunks-folder'
+            )
+                ->setUniqueIdentifier('unique-identifier')
+                ->setDomain('http://example.com') //to set cors header
+                ->checkDublicateFile(function($uuid) { //closure function to check file is duplicate or not it should get $uuid as input and return boolean. uuid is a video unique hash
+                	return false;
+                })
+                ->upload('path-to-upload-directory');
+```
+
+## Custome file name
+
+If want to set output file name try use setUploadName() function before fire finishUpload() function:
+```PHP
+$uploadResult = $uploadHandeler->setUploadName('my-name.mp4');
+```
+If don't set name your file name be with structur YYYY_m_d_hashname.mp4 and in final resutl generated name will be return.
+
+## Check and generate output directory
+If want to script make output directory automatically just set it:
+```PHP
+$uploadResult = $uploadHandeler->checkAndGenerateOutputDirectory();
+```
+
+## License
+
+hls-video-generater is licensed under the [GPLv3 License](http://opensource.org/licenses/GPL).
---
+++ src/FineUploader.php
@@ -0,0 +1,188 @@
+<?php
+namespace MehrdadDadkhah\Video;
+
+class FineUploader
+{
+    private $uploader;
+    private $requestMethod;
+    private $uniqueIdentifier;
+    private $checkDuplicate = false;
+    private $checkDublicateFileMethod;
+    private $domain;
+
+    public function __construct()
+    {
+        $this->uploader       = new ChunksUploader();
+        $this->requestMethod  = $this->get_request_method();
+        $this->requestHeaders = $this->parseRequestHeaders();
+    }
+
+    public function setConfigs(int $sizeLimit, string $inputName, string $chunksFolder)
+    {
+        $this->uploader->setMainFileName($_REQUEST['qqfilename'])
+            ->setFileTotalSize($_REQUEST['qqtotalfilesize'])
+            ->setInputName($inputName)
+            ->setChunksFolderPath($chunksFolder);
+
+        return $this;
+    }
+
+    public function setUniqueIdentifier(string $identifier)
+    {
+        $this->uniqueIdentifier = $identifier;
+
+        $this->uploader->setUniqueIdentifier($this->uniqueIdentifier);
+
+        return $this;
+    }
+
+    public function setUploadName(strign $fileName)
+    {
+        $this->uploader->setUploadName($fileName);
+
+        return $this;
+    }
+
+    public function checkAndGenerateOutputDirectory()
+    {
+        $this->uploader->checkAndGenerateOutputDirectory();
+
+        return $this;
+    }
+
+    public function upload($uploadDirectory)
+    {
+        if ($this->requestMethod != "POST") {
+            header("HTTP/1.0 405 Method Not Allowed");
+            return;
+        }
+
+        $this->handleCorsRequest();
+        header("Content-Type: text/plain");
+
+        // Assumes you have a chunking.success.endpoint set to point here with a query parameter of "done".
+        // For example: /myserver/handlers/endpoint.php?done
+        if (isset($_GET["done"])) {
+            $result = $this->uploader->setUploadDirectory($uploadDirectory)
+                ->finishUpload();
+
+            $result['success'] = $result['status'];
+            $result['status']  = ($result['status'] ? 0 : 1);
+
+            if ($result['status'] && $this->isDublicate($result['uuid'])) {
+                unlink($uploadDirectory . '/' . $result['file']);
+
+                $result = [
+                    'error'    => 5,
+                    "uuid"     => $result['uuid'],
+                    'status'   => -5,
+                    'messages' => 'duplicated',
+                ];
+            }
+
+            // return $result;
+            echo json_encode($result);
+
+        }
+        // Handles upload requests
+        else {
+
+            // Call handleUpload() with the name of the folder, relative to PHP's getcwd()
+            $result = $this->uploader->uploadChunk($_REQUEST['qqpartindex']);
+
+            $result['success'] = $result['status'];
+
+            // To return a name used for uploaded file you can use the following line.
+
+            // iframe uploads require the content-type to be 'text/html' and
+            // return some JSON along with self-executing javascript (iframe.ss.response)
+            // that will parse the JSON and pass it along to Fine Uploader via
+            // window.postMessage
+            if ($this->checkIframe()) {
+                header("Content-Type: text/html");
+
+                echo json_encode($result) . "<script src='http://jabeh.com/assets/js/iframe.xss.response.js'></script>";
+
+            } else {
+                echo json_encode($result);
+                // return $result;
+            }
+
+        }
+    }
+
+    private function checkIframe(): bool
+    {
+        if (!isset($this->requestHeaders['X-Requested-With']) || $this->requestHeaders['X-Requested-With'] != "XMLHttpRequest") {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function get_request_method()
+    {
+        global $HTTP_RAW_POST_DATA;
+        // This should only evaluate to true if the Content-Type is undefined
+        // or unrecognized, such as when XDomainRequest has been used to
+        // send the request.
+        if (isset($HTTP_RAW_POST_DATA)) {
+            parse_str($HTTP_RAW_POST_DATA, $_POST);
+        }
+        if (isset($_POST["_method"]) && $_POST["_method"] != null) {
+            return $_POST["_method"];
+        }
+        return $_SERVER["REQUEST_METHOD"];
+    }
+
+    private function parseRequestHeaders()
+    {
+        $headers = array();
+        foreach ($_SERVER as $key => $value) {
+            if (substr($key, 0, 5) != 'HTTP_') {
+                continue;
+            }
+            $header           = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+            $headers[$header] = $value;
+        }
+        return $headers;
+    }
+
+    private function handleCorsRequest()
+    {
+        header('Access-Control-Allow-Origin: ' . $this->domain);
+    }
+
+    public function setDomain(string $domain)
+    {
+        $this->domain = $domain;
+
+        return $this;
+    }
+
+    /*
+     * handle pre-flighted requests. Needed for CORS operation
+     */
+    private function handlePreflight()
+    {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Headers: Content-Type, X-Requested-With, Cache-Control");
+    }
+
+    public function checkDublicateFile($method)
+    {
+        $this->checkDuplicate = true;
+
+        $this->checkDublicateFileMethod = $method;
+
+        return $this;
+    }
+
+    private function checkDuplicate(strign $uuid): bool
+    {
+        $method = $this->checkDublicateFileMethod;
+        return $method($uuid);
+    }
+}
